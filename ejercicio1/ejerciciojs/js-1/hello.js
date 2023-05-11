f5423444c2dd822a8acd8914f3aed4257357f8e3
esteban <jhonespulgarin@gmail.com>
2023-05-10 20:39:38 -0500

    Nuevo commit

---
+++ ejercicio1/ejerciciojs/js-1/hello.js
@@ -0,0 +1,8 @@
+/*En un archivo llamado hello.js crear una función llamada hola que reciba un argumento 
+(una cadena de texto) y retorne “Hola ”
+ seguido del argumento y un signo de exclamación, debes ejecutar tu archivo usando node.js node hello.js*/
+
+ function hola  (nombre){
+    console.log(hola("jhon" + "!"));
+    
+ }
